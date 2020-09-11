d215c84ba2a1f0b36cb58ce390c767c2cef5fd01
Vismay Tiwari <61677369+vismaytiwari@users.noreply.github.com>
2020-09-11 10:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
 ﻿from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
 import glob
 import re
 import numpy as np
-
-# Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
-
-# Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-
-# Define a flask app
 app = Flask(__name__)
-
-# Model saved with Keras model.save()
 MODEL_PATH = 'models/my_model.h5'
-
-#Load your trained model
 model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary to make everything ready to run on the GPU ahead of time
+model._make_predict_function()
 print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#print('Model loaded. Check http://127.0.0.1:5000/')
-
-
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(50,50)) #target_size must agree with what the trained model expects!!
 
@@ -44,14 +25,10 @@ def model_predict(img_path, model):
     preds = model.predict(img)
     pred = np.argmax(preds,axis = 1)
     return pred
-
-
 @app.route('/', methods=['GET'])
 def index():
     # Main page
     return render_template('index.html')
-
-
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -75,12 +52,6 @@ def upload():
             return str1
         else:
             return str2
-    return 'hey'
-
-    #this section is used by gunicorn to serve the app on Heroku
+    return('working')
 if __name__ == '__main__':
         app.run()
-    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
-    # Serve the app with gevent 
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
