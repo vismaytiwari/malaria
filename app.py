099a87c0abc78c694f788d5eb82fdbe101889dad
Vismay Tiwari <61677369+vismaytiwari@users.noreply.github.com>
2020-09-11 11:09:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ def upload():
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.dirname(__file__)
-        #file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
-        file_path = join(dirname(realpath(__file__)), '/static/uploads/..')
+        file_path = os.path.join(basepath, '/uploads', secure_filename(f.filename))
         f.save(file_path)
         pred = model_predict(file_path, model)
         str1 = 'Malaria Parasitized'
